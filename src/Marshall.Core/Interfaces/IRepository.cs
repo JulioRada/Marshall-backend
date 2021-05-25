5d87492c461e5fb941d74acda3cf5ed2cae1fe3b
Eliana <gellyeliana2016@gmail.com>
2021-05-25 00:26:36 -0400

    Adding Marshall core

---
+++ src/Marshall.Core/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Marshall.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marshall.Core.Interfaces
+{
+    public interface IRepository<in T> : IDisposable where T : IEntity
+    {
+        int SaveChanges();
+        void Add(T entity);
+    }
+}
