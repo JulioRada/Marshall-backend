c8d4906a335e9eaaa313a838d70d7513a99b6384
Eliana <gellyeliana2016@gmail.com>
2021-05-25 00:32:48 -0400

    Adding migration tables

---
+++ src/Marshall.Infrastructure/Migrations/20210525042147_Initial.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Marshall.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Division",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Updated = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Division", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Office",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Updated = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Office", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Position",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Updated = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Position", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salary",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Updated = table.Column<DateTime>(nullable: true),
+                    Year = table.Column<int>(nullable: false),
+                    Month = table.Column<int>(nullable: false),
+                    OfficeId = table.Column<int>(nullable: false),
+                    EmployeeCode = table.Column<string>(nullable: true),
+                    EmployeeName = table.Column<string>(nullable: true),
+                    EmployeeSurname = table.Column<string>(nullable: true),
+                    DivisionId = table.Column<int>(nullable: false),
+                    PositionId = table.Column<int>(nullable: false),
+                    Grade = table.Column<int>(nullable: false),
+                    BeginDate = table.Column<DateTime>(nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    IdentificationNumber = table.Column<string>(nullable: true),
+                    BaseSalary = table.Column<float>(nullable: false),
+                    ProductionBonus = table.Column<float>(nullable: false),
+                    CompensationBonus = table.Column<float>(nullable: false),
+                    Commission = table.Column<float>(nullable: false),
+                    Contributions = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salary", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Salary_Division_DivisionId",
+                        column: x => x.DivisionId,
+                        principalTable: "Division",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Salary_Office_OfficeId",
+                        column: x => x.OfficeId,
+                        principalTable: "Office",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Salary_Position_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Position",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Salary_DivisionId",
+                table: "Salary",
+                column: "DivisionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Salary_OfficeId",
+                table: "Salary",
+                column: "OfficeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Salary_PositionId",
+                table: "Salary",
+                column: "PositionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Salary");
+
+            migrationBuilder.DropTable(
+                name: "Division");
+
+            migrationBuilder.DropTable(
+                name: "Office");
+
+            migrationBuilder.DropTable(
+                name: "Position");
+        }
+    }
+}
