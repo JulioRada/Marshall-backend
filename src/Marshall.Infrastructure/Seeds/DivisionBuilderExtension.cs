d2b3a03e67f770ce343d700837bf3b18257235d3
Eliana <gellyeliana2016@gmail.com>
2021-05-25 01:09:23 -0400

    Adding seed generation extension

---
+++ src/Marshall.Infrastructure/Seeds/DivisionBuilderExtension.cs
@@ -0,0 +1,22 @@
+﻿using Marshall.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marshall.Infrastructure.Seeds
+{
+    public static class DivisionBuilderExtension
+    {
+        public static void SeedDivision(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Division>()
+                .HasData(
+                    new Division { Id = 1, Name = "Operation" },
+                    new Division { Id = 2, Name = "Sales" },
+                    new Division { Id = 3, Name = "Marketing" },
+                    new Division { Id = 4, Name = "Customer care" }
+                );
+        }
+    }
+}
---
+++ src/Marshall.Infrastructure/Seeds/SalaryBuilderExtension.cs
@@ -0,0 +1,26 @@
+﻿using Marshall.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marshall.Infrastructure.Seeds
+{
+    public static class SalaryBuilderExtension
+    {
+        public static void SeedSalary(this ModelBuilder modelBuilder)
+        {
+            /*modelBuilder.Entity<Salary>()
+                .HasData(
+                    new Salary { Id = 1, Name = "Cargo manager" },
+                    new Salary { Id = 2, Name = "Head manager" },
+                    new Salary { Id = 3, Name = "Cargo assistant" },
+                    new Salary { Id = 4, Name = "Sales manager" },
+                    new Salary { Id = 5, Name = "Account executive" },
+                    new Salary { Id = 6, Name = "Marketing assistant" },
+                    new Salary { Id = 7, Name = "Customer director" },
+                    new Salary { Id = 8, Name = "Customer assistant" }
+                );*/
+        }
+    }
+}
