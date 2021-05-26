1530971b8603676a7105281176aff48fe2ecd833
Eliana <gellyeliana2016@gmail.com>
2021-05-26 03:32:37 -0400

    Adding cors and newtonsoft

---
+++ src/Marshall.Application/Cors/Cors.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marshall.Application.Cors
+{
+    public static class Cors
+    {
+        public static void AddAppCors(this IServiceCollection services)
+        {
+            services.AddCors();
+        }
+        public static void UseAppCors(this IApplicationBuilder app)
+        {
+            app.UseCors(builder => builder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .SetIsOriginAllowed((host) => true)
+                .AllowCredentials()
+            );
+        }
+    }
+}
--- src/Marshall.IoC/MarshallModule.cs
+++ src/Marshall.IoC/MarshallModule.cs
@@ -4,12 +4,14 @@ using Marshall.Application.Position;
 using Marshall.Domain.Interfaces.Repositories;
 using Marshall.Infrastructure.Context;
 using Marshall.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Marshall.Application.Cors;
 
 namespace Marshall.IoC
 {
@@ -26,10 +28,22 @@ namespace Marshall.IoC
             services.AddScoped<IOfficeQueries, OfficeQueries>();
             services.AddScoped<IDivisionQueries, DivisionQueries>();
             services.AddScoped<IPositionQueries, PositionQueries>();
+
+            // Config App
+            services.AddAppCors();
         }
         public static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<MarshallContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+        public static void AddControllersWithNewtonsoft(this IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson();
+        }
+        public static void Register(this IApplicationBuilder app)
+        {
+            // Cors
+            app.UseAppCors();
         }
     }
 }
